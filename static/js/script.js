ad8dd30d0c71d944d1be78701a1cb6300cb909e3
turalmz <turalmz@gmail.com>
2019-05-01 14:59:24 -0700

    js added

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,38 @@
-/*<input type="text" id="a" value="5">
-<script>*/
- var x = document.getElementById("a").value;
- console.log(x);
+
+// var x = document.getElementById("a").value;
+// console.log(x);
+
+window.onload = function(e){
+
+ var table = document.getElementById("result_list");
+
+ var trs = document.querySelectorAll('#result_list tbody tr');
 
 
+
+for(var i = 0; i < table.tBodies.length; i++) {
+  var tbody =  table.tBodies[i];
+  for (var j = 0; j < tbody.rows.length; j++) {
+    var row = tbody.rows[j];
+    console.log(row);
+    for (var k = 0; k < row.cells.length; k++) {
+      var cell = row.cells[k];
+      console.log(cell);
+    }
+  }
+}
+
+
+
+ for(var i = 0; i < trs.length; i++) {
+console.log(trs[i]);
+}
+
+
+}
+
+
+/*
 for(var i=1; i<=31; i++) {
 
 if(i==x||i==x+7||i==x+14||i==x+21||i==x+28)
@@ -11,5 +40,4 @@ var element = document.getElementsById("day"+i);
  element .style.backgroundColor = "red";
 
 }
-/*
-</script>*/
+*/
